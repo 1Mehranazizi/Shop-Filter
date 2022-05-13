81e160e14db94c247b781ec6d1746beef8cdcd53
Mehran <info.mehranazizi@yahoo.com>
2022-05-13 03:18:31 -0700

    Show products in page

--- src/Shop.js
+++ src/Shop.js
@@ -1,17 +1,31 @@
 //Components
-import Search from './components/Search';
+import Search from "./components/Search";
+import Product from "./components/Product";
 
 //Styles
 import styles from "./Shop.module.css";
 
-import React from 'react';
+//Data
+import { products } from "./data/data";
+
+import React from "react";
+
+console.log(products);
 
 const Shop = () => {
-    return (
-        <div className={styles.container}>
-            <Search />
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <Search />
+      <div className={styles.row}>
+        <aside></aside>
+        <main className={styles.products}>
+          {products.map((product) => (
+            <Product key={product.id} data={product} />
+          ))}
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
---
+++ src/components/Product.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+//Styles
+import styles from "./Product.module.css";
+
+//Icons
+import HeartIcon from "../assets/img/Icon heart.svg";
+import ShareIcon from "../assets/img/share.svg";
+
+const Product = ({data}) => {
+  return (
+    <div className={styles.product}>
+      <div className={styles.cardImage}>
+        <div className={styles.headerButton}>
+          <span className={styles.addToFavorite}>
+              <img src={HeartIcon} alt="heart" />
+          </span>
+          <span className={styles.share}>
+              <img src={ShareIcon} alt="share" />
+          </span>
+        </div>
+        <img src={data.image} alt={data.title} />
+      </div>
+      <div className={styles.cardBody}>
+          <h3 className={styles.title}>{data.title}</h3>
+          <p className={styles.price}>{data.price.toLocaleString()} تومان</p>
+      </div>
+      <button className={styles.addToCard}>افزودن به سبد خرید</button>
+    </div>
+  );
+};
+
+export default Product;
